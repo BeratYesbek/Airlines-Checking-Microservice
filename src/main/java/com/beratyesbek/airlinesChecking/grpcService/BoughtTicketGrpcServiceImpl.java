bbf14876a9acd370bdc135f53af4888fd49ef3ec
Feanor <beratyesbek@gmail.com>
2022-12-05 20:42:48 +0300

    Grpc method fix

--- src/main/java/com/beratyesbek/airlinesChecking/grpcService/BoughtTicketGrpcServiceImpl.java
+++ src/main/java/com/beratyesbek/airlinesChecking/grpcService/BoughtTicketGrpcServiceImpl.java
@@ -3,8 +3,8 @@ package com.beratyesbek.airlinesChecking.grpcService;
 import com.beratyesbek.airlinesChecking.dao.BoughtTicketDao;
 import com.beratyesbek.airlinesChecking.models.BoughtTicket;
 import com.beratyesbek.grpc.BoughtTicketRequest;
+import com.beratyesbek.grpc.BoughtTicketResponse;
 import com.beratyesbek.grpc.BoughtTicketServiceGrpc;
-import com.beratyesbek.grpc.empty;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.modelmapper.ModelMapper;
@@ -21,11 +21,11 @@ public class BoughtTicketGrpcServiceImpl extends BoughtTicketServiceGrpc.BoughtT
     }
 
     @Override
-    public void saveBoughtTicket(BoughtTicketRequest request, StreamObserver<empty> responseObserver) {
+    public void saveBoughtTicket(BoughtTicketRequest request, StreamObserver<BoughtTicketResponse> responseObserver) {
         BoughtTicket boughtTicket = modelMapper.map(request, BoughtTicket.class);
-        boughtTicketDao.save(boughtTicket);
+        boughtTicket = boughtTicketDao.save(boughtTicket);
+        responseObserver.onNext(BoughtTicketResponse.newBuilder().setId(boughtTicket.getBoughtTicketId()).build());
+        responseObserver.onCompleted();
         super.saveBoughtTicket(request, responseObserver);
     }
-
-
 }
